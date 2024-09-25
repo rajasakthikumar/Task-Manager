ee094a3e77288806ce374089d0c1c0e413921f5e
Rajasakthikumar <rajasakthikumarrajendran@gmail.com>
2024-09-25 10:36:03 +0530

    Modified and added new routes for task and status

--- routes/status.js
+++ routes/status.js
@@ -1,49 +1,53 @@
+// routes/statusRoutes.js
 const express = require('express');
 const { statusController } = require('../bootstrap');
 const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 console.log("Status router created in the routes");
-console.log(statusController);
 
-// Get all statuses (protected)
 router.get('/status', protect, (req, res, next) => {
     statusController.getAllStatus(req, res, next);
 });
 
-// Create a new status (protected)
+router.get('/status/deleted', protect, (req, res, next) => {
+    statusController.getDeletedStatuses(req, res, next);
+});
+
+router.get('/status/:id', protect, (req, res, next) => {
+    statusController.findStatusById(req, res, next);
+});
+
 router.post('/status', protect, (req, res, next) => {
     statusController.createStatus(req, res, next);
 });
 
-// Delete a status by ID (protected)
 router.delete('/status/:id', protect, (req, res, next) => {
     statusController.deleteStatus(req, res, next);
 });
 
-// Update/modify a status by ID (protected)
+router.post('/status/:id/restore', protect, (req, res, next) => {
+    statusController.restoreStatus(req, res, next);
+});
+
+router.delete('/status/:id/hard-delete', protect, (req, res, next) => {
+    statusController.hardDeleteStatus(req, res, next);
+});
+
 router.put('/status/:id', protect, (req, res, next) => {
     statusController.modifyStatus(req, res, next);
 });
 
-// Delete status by ID (alternative endpoint, protected)
-router.delete('/status/delete/:id', protect, (req, res, next) => {
-    statusController.deleteById(req, res, next);
-});
-
-// Add a status to the nextStatus array (protected)
-router.post('/status/n/:id/:nextStatusId', protect, (req, res, next) => {
+router.post('/status/:id/next-status', protect, (req, res, next) => {
     statusController.addNextStatus(req, res, next);
 });
 
-// Add a status to the previousStatus array (protected)
-router.post('/status/p/:id/:prevStatusId', protect, (req, res, next) => {
+router.post('/status/:id/prev-status', protect, (req, res, next) => {
     statusController.addPrevStatus(req, res, next);
 });
 
-// Find status by ID (protected)
-router.get('/status/:id', protect, (req, res, next) => {
-    statusController.findStatusById(req, res, next);
+router.get('/status/validate-transition/:currentStatusId/:nextStatusId', protect, (req, res, next) => {
+    statusController.validateTransition(req, res, next);
 });
 
 module.exports = router;
--- routes/task.js
+++ routes/task.js
@@ -1,3 +1,4 @@
+// routes/taskRoutes.js
 const express = require('express');
 const { taskController } = require('../bootstrap');
 const { protect } = require('../middleware/auth');
@@ -5,43 +6,96 @@ const router = express.Router();
 
 console.log("Task router created");
 
-// Get all tasks (protected)
 router.get('/tasks', protect, (req, res, next) => {
     taskController.getAllTasks(req, res, next);
 });
 
-// Get a task by ID (protected)
 router.get('/tasks/:id', protect, (req, res, next) => {
     taskController.getTaskById(req, res, next);
 });
 
-// Create a new task (protected)
 router.post('/tasks', protect, (req, res, next) => {
     taskController.createTask(req, res, next);
 });
 
-// Update task details (protected)
 router.put('/tasks/:id', protect, (req, res, next) => {
     taskController.updateTask(req, res, next);
 });
 
-// Update task status (protected)
 router.put('/tasks/:taskId/status/:statusId', protect, (req, res, next) => {
     taskController.updateTaskStatus(req, res, next);
 });
 
-// Delete a task by ID (protected)
 router.delete('/tasks/:id', protect, (req, res, next) => {
     taskController.deleteTask(req, res, next);
 });
 
-// Find tasks by status (protected)
-router.get('/tasks/status/:statusId', protect, (req, res, next) => {
-    taskController.findTasksByStatus(req, res, next);
+router.delete('/tasks', protect, (req, res, next) => {
+    taskController.deleteTasks(req, res, next);
+});
+
+router.post('/tasks/:id/assign', protect, (req, res, next) => {
+    taskController.assignTask(req, res, next);
 });
 
 router.post('/tasks/:taskId/comments', protect, (req, res, next) => {
     taskController.addComment(req, res, next);
+});
+
+router.get('/tasks/priority/:priority', protect, (req, res, next) => {
+    taskController.getTasksByPriority(req, res, next);
+});
+
+router.put('/tasks/:id/priority', protect, (req, res, next) => {
+    taskController.updateTaskPriority(req, res, next);
+});
+
+router.get('/tasks/assigned', protect, (req, res, next) => {
+    taskController.getTasksAssignedTo(req, res, next);
+});
+
+router.get('/tasks/created', protect, (req, res, next) => {
+    taskController.getTasksCreatedBy(req, res, next);
+});
+
+router.delete('/tasks/:id/soft-delete', protect, (req, res, next) => {
+    taskController.softDeleteTask(req, res, next);
+});
+
+router.post('/tasks/:id/restore', protect, (req, res, next) => {
+    taskController.restoreTask(req, res, next);
+});
+
+router.get('/tasks/deleted', protect, (req, res, next) => {
+    taskController.getDeletedTasks(req, res, next);
+});
+
+router.get('/tasks/status/:statusId', protect, (req, res, next) => {
+    taskController.findTasksByStatus(req, res, next);
+});
+
+router.get('/tasks/date-range', protect, (req, res, next) => {
+    taskController.getTasksByDateRange(req, res, next);
+});
+
+router.get('/tasks/search', protect, (req, res, next) => {
+    taskController.searchTasks(req, res, next);
+});
+
+router.get('/tasks/overdue', protect, (req, res, next) => {
+    taskController.getOverdueTasks(req, res, next);
+});
+
+router.get('/tasks/due-today', protect, (req, res, next) => {
+    taskController.getTasksDueToday(req, res, next);
+});
+
+router.put('/tasks/:id/unassign', protect, (req, res, next) => {
+    taskController.unassignTask(req, res, next);
+});
+
+router.put('/tasks/:id/end-date', protect, (req, res, next) => {
+    taskController.updateTaskEndDate(req, res, next);
 });
 
 module.exports = router;
